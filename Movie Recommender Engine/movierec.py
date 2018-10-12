fa57b1a0fd16045e0d5be51cb8554f25d84ca2bd
Abhilash Pal <abhilashpal8@gmail.com>
2018-10-12 15:00:39 +0530

    Adding Movie Rec Steps

---
+++ Movie Recommender Engine/movierec.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from ast import literal_eval
+
+#import data
+md = pd. read_csv('movies_metadata.csv')
