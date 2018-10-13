c0c543b9bfcbe5c24b97d5268bbd33c1a8093830
Sukrit Bhattacharya <sukritbhattacharya4498@gmail.com>
2018-10-13 06:13:30 +0530

    Update Meme Generation Engine/memes/memes.py

--- Meme Generation Engine/memes/memes.py
+++ Meme Generation Engine/memes/memes.py
@@ -3,13 +3,15 @@ import os
 import cv2
 
 import random
-a= random.randint(1,21)
+a= random.randint(1,20)
 print(a)
 
 a2=str(a) + ".jpg"
 
 img = cv2.imread(a2, 1)
-cv2.imshow('img',img)
+resized_image = cv2.resize(img, (500, 500))
+
+cv2.imshow('img',resized_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
